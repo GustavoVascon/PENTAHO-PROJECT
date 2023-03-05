0fbd2fa62e86383419daff0c7b5303af08c6e76b
GustavoVascon <gustavovasconcelosti@gmail.com>
2023-03-04 23:06:00 -0300

    PENTAHO

---
+++ Transformacoes/Bat/executar_job_principal_stage_dw.bat
@@ -0,0 +1,1 @@
+C:\Users\Gustavo\Desktop\data-integration\Kitchen.bat /file:"C:\Users\Gustavo\Desktop\Udemy - Pentaho Data Integration - ETL Essencial\Projeto Oficial\Transformacoes\job_principal_stage_dw.kjb"
